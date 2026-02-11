4403722450264ad5a9711258f58c7b67e3de36ac
yotsarunkia <yotsarun.kia@student.mahidol.edu>
2026-02-11 04:34:08 +0000

    main.cpp

--- monster.h
+++ monster.h
@@ -3,11 +3,32 @@
 
 class monster{
 private:
-	string name;
-	int hp,potion;
+    string name;
+    int hp,pot;
 public:
-	void Attack(monster &);
-          void heal();
-	monster(string, int,int);
+    void Attack(monster &);
+    void heal();
+    void display();
+    monster(string, int,int);
+    ~monster();
 };
+void monster::display()
+{
+    cout<<name<<endl;
+    cout<<"Health : "<<hp<<endl;
+    cout<<"Potions : "<<pot<<endl;
+    
+}
+
+monster::monster(string n="anonymous", int h=1, int p=1){
+    name=n;
+    hp=h;
+    pot=p;
+    cout<<"A wild "<<name<<" appears!"<<endl;
+}
+
+monster::~monster(){
+    cout<<name<<" is deleted."<<endl;
+	
+}
 #endif
